1050e433e7a12d1bfbd2a1aad7778e980306db6d
Yogita <radheyavrushali2@gmail.com>
2025-01-15 09:07:46 +0530

    Updated formikValidations
    completed the pending code

--- Practice Project/ReactB11Duplicate/my-react-tasks/src/formik-validation/formik-validations.jsx
+++ Practice Project/ReactB11Duplicate/my-react-tasks/src/formik-validation/formik-validations.jsx
@@ -1,33 +1,59 @@
 import { useFormik } from "formik";
 
 export default function FormikValidationsDemo() {
-    function validateUser(formData){
-        var errors={
-            UserName:'',
-            Age:'',
-            City:'',
-            Mobile:''
+    function validateUser(formData) {
+        var errors = {
+            UserName: '',
+            Age: '',
+            City: '',
+            Mobile: ''
         }
-if (formData.UserName==='') {
-    errors.UserName='UserName is required'
-}else{
-    if(formData.UserName.length<4){
-errors.UserName='UserName is too short'
-    }
-}
+
+        if (formData.UserName.length=== '') {
+            errors.UserName = 'UserName is required'
+        } else {
+            if (formData.UserName.length < 4) {
+                errors.UserName = 'UserName is too short'
+            }else {
+                errors.UserName = ''
+            }
+        }
+
+        if (parseInt(formData.Age, 10) <= 0) {
+            errors.Age = 'Age Required'
+        } else {
+            if (isNaN(formData.Age)) {
+                errors.Age = 'Age must be a number'
+            } else {
+                errors.Age = ''
+            }
+        }
+
+        if (formData.City === '-1') {
+            errors.City = 'Please Select your city'
+        } else {
+            errors.City = ''
+        }
+
+        if (formData.Mobile.match(/\+91\d{10}/)) {
+            errors.Mobile = ''
+        } else {
+            errors.Mobile = 'Enter valid mobile number'
+        }
 
         return errors;
-
     }
+
+
     const formik = useFormik({
         // you must use same values as you have mentioned in name of element
         initialValues: {
             UserName: '',
             Age: 0,
-            City: '',
-            Mobile:0
+            City: '-1',
+            Mobile: ''
         },
-        validate:validateUser,
+        validate: validateUser,
         onSubmit: (user) => {
             console.log(user);
         }
@@ -39,29 +65,29 @@ errors.UserName='UserName is too short'
                 <dl>
                     <dt>UserName</dt>
                     <dd><input type="text" name="UserName" onChange={formik.handleChange} /></dd>
+                    <dd className='text-danger'>{formik.errors.UserName}</dd>
 
                     <dt>Age</dt>
-                    <dd><input type="number" name="Age" onChange={formik.handleChange} /></dd>
-
+                    <dd><input type="text" name="Age" onChange={formik.handleChange} /></dd>
+                    <dd className='text-danger'>{formik.errors.Age}</dd>
+                    
                     <dt>City</dt>
                     <dd>
                         <select name="City" onChange={formik.handleChange}>
-                            <option>Select City</option>
-                            <option>Pandharpur</option>
-                            <option>Pune</option>
-                            <option>Hyderabad</option>
+                            <option value="-1">Select City</option>
+                            <option value="Pandharpur">Pandharpur</option>
+                            <option value="Pune">Pune</option>
+                            <option value="Hyderabad">Hyderabad</option>
                         </select>
                     </dd>
-
+                    <dd className='text-danger'>{formik.errors.City}</dd>
+                    
                     <dt>Mobile</dt>
-                    <dd>
-                        <input type="number"  name="Mobile" onChange={formik.handleChange}/>
-                    </dd>                    
-
+                    <dd><input type="number" name="Mobile" onChange={formik.handleChange} /></dd>
+                    <dd className='text-danger'>{formik.errors.Mobile}</dd>
                 </dl>
                 <button type="submit" >Submit</button>
             </form>
-
         </div>
     )
 }
