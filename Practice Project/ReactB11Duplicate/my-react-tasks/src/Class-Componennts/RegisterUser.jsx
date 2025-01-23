d71d432fba8de88cbfcdbd5487e5878033633f61
Yogita <radheyavrushali2@gmail.com>
2025-01-23 12:43:37 +0530

    How to write Class Component

--- Practice Project/ReactB11Duplicate/my-react-tasks/src/Class-Componennts/RegisterUser.jsx
+++ Practice Project/ReactB11Duplicate/my-react-tasks/src/Class-Componennts/RegisterUser.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 
 export default class RegisterUser extends Component {
-    constructor(){
-        super();
-    }
+
+  constructor(props) {
+    super();
+  }
+
+  
   render() {
     return (
       <div>
