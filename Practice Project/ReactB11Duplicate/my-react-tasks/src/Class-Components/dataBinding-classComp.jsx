1990a0816c919fd0a18b572d391839a2212b1317
Yogita <radheyavrushali2@gmail.com>
2025-02-02 21:42:44 +0530

    Git commands trial

--- Practice Project/ReactB11Duplicate/my-react-tasks/src/Class-Components/dataBinding-classComp.jsx
+++ Practice Project/ReactB11Duplicate/my-react-tasks/src/Class-Components/dataBinding-classComp.jsx
@@ -4,12 +4,14 @@ export default class DataBindingClassComp extends Component {
     constructor(props) {
         super();
     }
-    UserName = "john";
+   
+    UserName="Yogita"
     render() {
         return (
             <div className='container-fluid'>
                 <p>Hello...! {this.UserName}</p>
                 <p>This is not recommended to store data in property.We recommend to store data in state</p>
+                <p>Git check</p>
             </div>
         )
     }
