6c567e1f032d878694aa835a00d46f5fd294677e
Yogita <radheyavrushali2@gmail.com>
2025-02-24 22:46:00 +0530

    Update FakestoreHome
    To get data from cookies and display data. remove Cookies implemented

--- Practice Project/ReactB11Duplicate/my-react-tasks/src/Fakestore-Routing/FakestoreHome.jsx
+++ Practice Project/ReactB11Duplicate/my-react-tasks/src/Fakestore-Routing/FakestoreHome.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
 
 const FakestoreHome = () => {
   const [categories, setCategories] = useState([])
 
+  const [cookies, setCookies, removeCookie] = useCookies(['userid']);
+
+  const navigate=useNavigate();
+  
   useEffect(() => {
     axios.get(`https://fakestoreapi.com/products/categories/`)
       .then(response => {
         setCategories(response.data);
       })
   }, [])
+
+  function handleSignout(){
+    removeCookie('userid')
+    navigate('/login')
+  }
   return (
     <div>
-      <h2>FakeStore Home</h2>
+      <h2 className='d-flex justify-content-between'> <span> FakeStore Home</span> <span>{cookies['userid']}</span></h2>
 
       <ul className='list-unstyled '>
         {
@@ -24,7 +34,9 @@ const FakestoreHome = () => {
               </Link>
             </li>)
         }
-        <li><Link to='/login' className='btn btn-warning w-25 text-decoration-none my-2 '>USER LOGIN</Link></li>
+        {
+          (cookies['userid'])?<button onClick={handleSignout} className='btn btn-danger w-25'>Signout</button>:<li><Link to='/login' className='btn btn-warning w-25 text-decoration-none my-2 '>USER LOGIN</Link></li>
+        }
       </ul>
 
     </div>
