d15827199758b69f614f89cec8ec77a3ef24dce4
Yogita <radheyavrushali2@gmail.com>
2025-02-24 23:59:01 +0530

    update 2nd on products page

--- Practice Project/ReactB11Duplicate/my-react-tasks/src/Fakestore-Routing/FakestoreProducts.jsx
+++ Practice Project/ReactB11Duplicate/my-react-tasks/src/Fakestore-Routing/FakestoreProducts.jsx
@@ -1,23 +1,32 @@
 import React, { useEffect } from 'react'
-import { Link, Outlet, useParams } from 'react-router-dom'
+import { Link, Outlet, useParams,useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 import axios from 'axios';
+import { useCookies } from 'react-cookie'
 
 const FakestoreProducts = () => {
   let params = useParams();
 
   const [products, setProducts] = useState([{ id: 0, title: '', image: '', description: '', category: '', rating: { rate: 0, count: 0 } }])
 
+   const [cookies, setCookies, removeCookie] = useCookies(['userid']);
+
+   const navigate=useNavigate();
   useEffect(() => {
+    if(cookies['userid']){
     axios.get(`https://fakestoreapi.com/products/category/${params.category}`)
       .then(response => {
         setProducts(response.data)
       })
+    }
+    else{
+      navigate('/login')
+    }
   })
 
   return (
     <div>
-      <h3>Products</h3>
+      <h3 className='d-flex justify-content-between'><span>Products </span> <span>{cookies['userid']}</span></h3>
       <div className='row'>
         <div className="col-4">
           <div className='d-flex flex-wrap'>
